47ec29e21c67e464ee1ce61f8bd38a7bd888c2d5
Dan Dumitriu <dandumitriu33@gmail.com>
2020-07-10 08:55:57 +0300

    Add StockAPIService Test 1 mock - successful

--- StockTraderTests/StockTrader/StockAPIServiceTest.cs
+++ StockTraderTests/StockTrader/StockAPIServiceTest.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
-using NSubstitute;
+using Autofac.Extras.Moq;
+using stock_trader_app_DI_csharp.StockTrader;
 
 namespace stockTrader
 {
@@ -8,7 +9,21 @@ namespace stockTrader
         [Test] // everything works
         public void TestGetPriceNormalValues()
         {
-            
+            using (var mock = AutoMock.GetLoose())
+            {
+                var mockResponse = "{\n\"symbol\" : \"TEST\",\n\"price\" : 383.08000000,\n\"volume\" : 18086123\n}";
+                mock.Mock<IRemoteURLReader>()
+                    .Setup(x => x.ReadFromUrl($"https://financialmodelingprep.com/api/v3/stock/real-time-price/TEST?apikey=demo"))
+                    .Returns(mockResponse);
+
+                var cls = mock.Create<StockAPIService>();
+                var expected = 383.08000000;
+
+                var actual = cls.GetPrice("TEST");
+
+                Assert.AreEqual(expected, actual);
+                Assert.AreNotEqual(384.0800, actual);
+            }
         }
 
         [Test] // readFromURL threw an exception
