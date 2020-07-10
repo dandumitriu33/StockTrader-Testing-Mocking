0fe69fde7f181d4a2b687d8659caa5470aedd63d
Dan Dumitriu <dandumitriu33@gmail.com>
2020-07-10 10:19:13 +0300

    Add 2nd test - readFromURL throws exception

--- StockTraderTests/StockTrader/StockAPIServiceTest.cs
+++ StockTraderTests/StockTrader/StockAPIServiceTest.cs
@@ -1,6 +1,8 @@
 using NUnit.Framework;
 using Autofac.Extras.Moq;
 using stock_trader_app_DI_csharp.StockTrader;
+using System;
+using NSubstitute.ExceptionExtensions;
 
 namespace stockTrader
 {
@@ -29,6 +31,23 @@ namespace stockTrader
         [Test] // readFromURL threw an exception
         public void TestGetPriceServerDown()
         {
+            using (var mock = AutoMock.GetLoose())
+            {
+                mock.Mock<IRemoteURLReader>()
+                    .Setup(x => x.ReadFromUrl($"https://financialmodelingprep.com/api/v3/stock/real-time-price/TEST?apikey=demo"))
+                    .Throws(new ApplicationException("Error"));
+
+                var cls = mock.Create<StockAPIService>();
+
+                Assert.Throws<ApplicationException>(() => mock.Create<StockAPIService>().GetPrice("TEST"));
+                
+                //var actual = cls.GetPrice("TEST");
+
+                ////Assert.Throws<ApplicationException>(actual);
+                ////Assert.That(() => cls.GetPrice("TEST"), Throws.TypeOf<ApplicationException>());
+                //var ex = Assert.Throws<ApplicationException>(() => cls.GetPrice("TEST"));
+                //Assert.That(ex.Message, Is.EqualTo("Error"));
+            }
         }
 
         [Test] // readFromURL returned wrong JSON
